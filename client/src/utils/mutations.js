6cef1fa5b971dc59d83c592a9bfd5d78d8ccc6cd
ItsGeeBee <gracelbrown@hotmail.com>
2022-10-06 16:58:37 +1100

    Completed resolvers and mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,51 @@
+import { gql } from '@apollo/client';
+
+export const CREATE_USER = gql`
+mutation createUser($username: String!, $email: String!, $password: String!){
+    createUser(username:$username, email: $email, password: $password){
+        token
+        user{
+            _id
+            username
+        }
+    }
+}`
+
+export const LOGIN_USER = gql`
+mutation login ($email: String!, $password: String!) {
+    login( email: $email, password: $password) {
+    token
+    user {
+        _id
+        username
+        }
+    } 
+}`
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($title: String, $bookId: String, $description: String, $authors: [String], $image: String){
+        saveBook(title: $title, bookId: $bookId, description: $description, authors: $authors, image: $image){
+            _id
+            username
+            email
+            savedBooks{
+                title
+                bookId
+                description
+            }
+        }
+    }`
+
+export const DELETE_BOOK = gql`
+    mutation deleteBook($bookId: String!){
+        deleteBook(bookId: $bookId){
+            _id
+            username
+            email
+            savedBooks{
+                title
+                bookId
+                description
+            }
+        }
+    }`
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -19,3 +19,42 @@ const resolvers = {
         const token = signToken(user);
         return { token, user };
       },
+
+      login: async (parent, { email, password }) => {
+        const user = await User.findOne({ email });
+        if (!user) {
+            throw new AuthenticationError('Invalid credentials')
+        }
+        const correctPw = await user.isCorrectPassword(password);
+        if (!correctPw) {
+            throw new AuthenticationError('Invalid credentials')
+        }
+        const token = signToken(user);
+        return {user, token};
+    },
+    saveBook: async (parent, { book }, context) => {
+        if (context.user) {
+            const updatedUser = await User.findByIdAndUpdate(
+                { _id: context.user._id},
+                { $addToSet: { savedBooks: book } },
+                { new: true }
+            )
+            return updatedUser;
+        }
+        throw new AuthenticationError('You need to be logged in first!')
+    },
+    removeBook: async (parent, { bookId }, context) => {
+        if (context.user) {
+            const updatedUser = await User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $pull: { savedBooks: { bookId: bookId }}},
+                { new: true }
+            )
+            return updatedUser;
+        }
+        throw new AuthenticationError('You need to be logged in first!');
+    }
+}
+};
+
+module.exports = resolvers;
