bf0ca715d0d7f4061b805e1f03808c8b4004b216
ItsGeeBee <gracelbrown@hotmail.com>
2022-10-08 12:43:26 +1100

    refactored loginForm and SignupForm

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
 import { ADD_USER } from '../utils/mutations';
 import {useMutation} from '@apollo/client';
 import Auth from '../utils/auth';
@@ -12,6 +11,7 @@ const SignupForm = () => {
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+  const [addUser, { error }] = useMutation(ADD_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -29,18 +29,13 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      const { data } = await addUser({
+        variables: { ...userFormData },
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      Auth.login(data.addUser.token);
     } catch (err) {
       console.error(err);
-      setShowAlert(true);
     }
 
     setUserFormData({
@@ -103,6 +98,7 @@ const SignupForm = () => {
           variant='success'>
           Submit
         </Button>
+        {error && <div> Sign up failed</div>}
       </Form>
     </>
   );
